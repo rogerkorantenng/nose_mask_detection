50105e785b2bfe4e8ce0a9a8c35f1f5c0ca7422e
Rogerkoranteng <77711618+rogerkorantenng@users.noreply.github.com>
2022-02-24 14:08:34 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ import os
 from werkzeug.utils import secure_filename
 from urllib.request import Request
 from flask import Flask, render_template, Response, request, redirect, flash
-from Myfunctions import *
+#from Myfunctions import *
 import urllib
 import secrets
 #import cv2
